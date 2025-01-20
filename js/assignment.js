e3a2c079358d530dea11cf651db34c067fa7ef66
Jessica Plemmons <jessica.plemmons@hotmail.com>
2025-01-19 23:16:40 -0500

    Trying to understand this

--- js/assignment.js
+++ js/assignment.js
@@ -1,8 +1,10 @@
 "use strict";
 
 const submissionBtn = document.getElementById("submission-btn");
-const remainderElement = document.getElementById("remainder");
+const remainderElement = document.getElementById
+("remainder");
 const checkIfEvenElement = document.getElementById("check-if-even");
 const lostNumbersElement = document.getElementById("lost-numbers");
 
 const lostNumbers = [4, 8, 15, 16, 23, 42];
+
--- js/index.js
+++ js/index.js
@@ -2,3 +2,17 @@
 // that are not declared
 "use strict"
 
+/*
+function greetings() {
+    return 'Hello, World!';
+}
+let message = greetings();
+console.log(message);
+
+
+function sayHello() {
+    return 'Goodbye, World!';
+}
+let message = sayHello();
+console.log(message);
+*/
